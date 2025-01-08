6be73e2e7c189c1a3ac1be102935553368c03754
EvanJannot <ejannot@ensc.fr>
2025-01-08 00:01:19 -0500

    feat: X axis date format

--- frontend/components/charts/LineChart.tsx
+++ frontend/components/charts/LineChart.tsx
@@ -4,8 +4,19 @@ import { LineChart, ResponsiveContainer, Line, CartesianGrid, XAxis, YAxis, Tool
 import { TransformedWeatherData } from '@/types/weather.types';
 
 interface LineChartComponentProps {
-  data: TransformedWeatherData[];  // Les données météorologiques à afficher
+  data: TransformedWeatherData[]; 
 }
+
+const formatDate = (timestamp: string) => {
+  const dateParts = timestamp.split(' ');
+  const date = dateParts[0].split('-'); 
+  const time = dateParts[1].split(':'); 
+
+  const formattedDate = `${date[1]}/${date[2]}`;
+  const formattedTime = `${time[0]}h`; 
+
+  return `${formattedDate} ${formattedTime}`;
+};
 
 const LineChartComponent: React.FC<LineChartComponentProps> = ({ data }) => {
   return (
@@ -13,7 +24,7 @@ const LineChartComponent: React.FC<LineChartComponentProps> = ({ data }) => {
       <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
         <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="date" />
+        <XAxis dataKey="date" tickFormatter={(value) => formatDate(value)} />
         <YAxis />
         <Tooltip />
       </LineChart>
